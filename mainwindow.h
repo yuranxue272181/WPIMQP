347793f501d60a95f31d5de27f79f08a8bac20d7
yuranxue272181 <xue272181@gmail.com>
2024-11-25 01:11:38 -0500

    redo ui

--- mainwindow.h
+++ mainwindow.h
@@ -106,12 +106,5 @@ signals:
     // void glInitialized();
 
 
-private:
-    float meanCal(std::shared_ptr<QVector<int>> grayValues);
-    float pixelNoiseCal(std::shared_ptr<QVector<int>> grayValues, float mean);
-    float calculateRowNoise(std::shared_ptr<QVector<int>> grayValues, int rowIndex, int selectedWidth);
-    float calculateAllRowNoise(std::shared_ptr<QVector<int>> grayValues, int selectedHeight, int selectedWidth);
-    float calculateColumnNoise(std::shared_ptr<QVector<int>> grayValues, int columnIndex, int selectedHeight, int selectedWidth) ;
-    float calculateAllColumnNoise(std::shared_ptr<QVector<int>> grayValues, int selectedHeight, int selectedWidth);
 };
 #endif // MAINWINDOW_H
